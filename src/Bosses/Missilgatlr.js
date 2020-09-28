27ab128e17e40db38686b0c4e4e01c8ef4736fba
goldenPiGames <goldenpigames@gmail.com>
2020-09-28 14:04:10 -0400

    Missilgatlr burst

--- src/Bosses/Missilgatlr.js
+++ src/Bosses/Missilgatlr.js
@@ -1,13 +1,29 @@
 function Missilgatlr() {
-	this.bulletCD = 20;
+	this.bulletCD = 0;
+	this.resting = 10;
 }
 Missilgatlr.prototype = Object.create(Boss.prototype);
-Missilgatlr.prototype.bulletRate = 12;
+Missilgatlr.prototype.bulletRate = 4;
+Missilgatlr.prototype.restTime = 48;
+Missilgatlr.prototype.burstSize = 16;
 Missilgatlr.prototype.update = function(game) {
 	//console.log(game.getRandomPlayer())
-	this.bulletCD--;
-	if (this.bulletCD <= 0) {
-		game.addBullet(new StraightBullet(WIDTH/2, HEIGHT/5, 5, new VectorPolar(6, angleCoordToObj(WIDTH/2, HEIGHT/5, game.getRandomPlayer()) + randomOffset(.1))));
-		this.bulletCD = this.bulletRate;
+	if (this.bulletsLeft > 0) {
+		console.log("bursting");
+		this.bulletCD--;
+		if (this.bulletCD <= 0) {
+			game.addBullet(new StraightBullet(WIDTH/2, HEIGHT/5, 5, new VectorPolar(6, angleCoordToObj(WIDTH/2, HEIGHT/5, game.getRandomPlayer()) + randomOffset(.1))));
+			this.bulletCD = this.bulletRate;
+			this.bulletsLeft--;
+			if (this.bulletsLeft <= 0) {
+				this.resting = this.restTime;
+			}
+		}
+	} else if (this.resting > 0) {
+		console.log("resting");
+		this.resting --;
+	} else {
+		console.log("time to burst");
+		this.bulletsLeft = this.burstSize;
 	}
 }
